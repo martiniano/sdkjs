a17e34182309d5cf35776317b58f374d3f2d87bb
Anderson Martiniano <andersonmartiniano@gmail.com>
2019-04-25 17:59:44 -0300

    New features.

--- word/custom/nuclearis.js
+++ word/custom/nuclearis.js
@@ -30,17 +30,70 @@ Asc['asc_docs_api'].prototype.nuclearis_redoSignatures = function()
     });
 };
 
+Asc['asc_docs_api'].prototype.nuclearis_InsertText = function(sText) 
+{
+    var me = this;
+
+    var logicDocument =  me.WordControl.m_oLogicDocument;
+    var paraRun = logicDocument.Get_DocumentPositionInfoForCollaborative();
+    paraRun.Class.AddText(sText, paraRun.Class.Content.length);
+    paraRun.Class.MoveCursorToEndPos(false);
+
+    //var oDocument = me.GetDocument();
+
+    //var oParagraph, oRun;
+    //oParagraph = logicDocument.GetCurrentParagraph();
+    //oParagraph = me.CreateParagraph();
+    //var oRun = me.CreateRun();
+    //oRun.AddText(sText);
+    //oParagraph.AddElement(oRun);
+    //var result = oDocument.InsertContent([oParagraph], true);
+    //console.log(result);
+   
+    me.asc_Recalculate();
+};
+
+Asc['asc_docs_api'].prototype.nuclearis_NewParagraph = function(sText) 
+{
+    var me = this;
+    var oDocument = me.GetDocument();
+    var oParagraph = me.CreateParagraph();
+    var oRun = me.CreateRun();
+    oRun.AddText(sText);
+    oParagraph.AddElement(oRun);
+    var result = oDocument.InsertContent([oParagraph], true);
+    
+    me.asc_Recalculate();
+};
+
+
+Asc['asc_docs_api'].prototype.nuclearis_AddLineBreak = function()
+{
+    var me = this;
+    if ( false === this.WordControl.m_oLogicDocument.Document_Is_SelectionLocked(AscCommon.changestype_Paragraph_Content) )
+    {
+        var Document = this.WordControl.m_oLogicDocument;
+
+        if ( null === Document.IsCursorInHyperlink(false) )
+        {
+            Document.Create_NewHistoryPoint();
+            Document.AddToParagraph( new ParaNewLine( para_NewLine ) );
+        }
+    }
+};
+
 
 /*
 function NuclearisCustomizations(){
-
 }
 
 NuclearisCustomizations.prototype.redoSignatures = function(){
-
 }
 */
 
 Asc['asc_docs_api'].prototype["nuclearis_redoSignatures"] = Asc['asc_docs_api'].prototype.nuclearis_redoSignatures;
+Asc['asc_docs_api'].prototype["nuclearis_InsertText"] = Asc['asc_docs_api'].prototype.nuclearis_InsertText;
+Asc['asc_docs_api'].prototype["nuclearis_NewParagraph"] = Asc['asc_docs_api'].prototype.nuclearis_NewParagraph;
+Asc['asc_docs_api'].prototype["nuclearis_AddLineBreak"]  = Asc['asc_docs_api'].prototype.nuclearis_AddLineBreak;
 //window['Asc']['asc_docs_api'].prototype["nuclearis_redoSignatures"] = window['Asc']['asc_docs_api'].prototype.nuclearis_redoSignatures;
  
